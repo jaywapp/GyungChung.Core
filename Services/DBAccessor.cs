2a9152de3a3810009392cc465cbabf5a6c65e201
Junyoung Park <jaywapp16@gmail.com>
2025-10-01 20:47:31 +0900

    update

---
+++ Services/DBAccessor.cs
@@ -0,0 +1,39 @@
+﻿using GyungChung.Core.Models;
+using MongoDB.Driver;
+
+namespace GyungChung.API.Services
+{
+    public class DBAccessor
+    {
+        private const string URL = "mongodb://192.168.0.101:27017/";
+        private const string DB_NAME = "GyungChung";
+
+        private const string COLLECTION_MEMBERS = "Members";
+        private const string COLLECTION_LOCATIONS = "Locations";
+        private const string COLLECTION_SCHEDULES = "Schedules";
+
+        private MongoClient _client;
+        private IMongoDatabase _db;
+        private IMongoCollection<Member> _memberCollection;
+        private IMongoCollection<Location> _locationCollection;
+        private IMongoCollection<Schedule> _scheduleCollection;
+
+        public DBAccessor()
+        {
+            _client = new MongoClient(URL);
+            _db = _client.GetDatabase(DB_NAME);
+
+            _memberCollection = _db.GetCollection<Member>(COLLECTION_MEMBERS);
+            _locationCollection = _db.GetCollection<Location>(COLLECTION_LOCATIONS);
+            _scheduleCollection = _db.GetCollection<Schedule>(COLLECTION_SCHEDULES);
+        }
+
+        public async Task<List<Member>> GetMembersAsync()
+        {
+            var fluent = _memberCollection.Find(Builders<Member>.Filter.Empty);
+            var members = await fluent.ToListAsync();
+
+            return members;
+        }
+    }
+}
---
+++ Services/MongoService.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+
+namespace GyungChung.API.Services
+{
+    public class MongoService<T>
+    {
+        private readonly IMongoCollection<T> _collection;
+
+        public MongoService(IMongoDatabase database, string collectionName)
+        {
+            _collection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task<List<T>> GetAllAsync() =>
+            await _collection.Find(_ => true).ToListAsync();
+
+        public async Task<T> GetByIdAsync(string idField, string idValue)
+        {
+            var filter = Builders<T>.Filter.Eq(idField, idValue);
+            return await _collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(T entity) =>
+            await _collection.InsertOneAsync(entity);
+
+        public async Task UpdateAsync(string idField, string idValue, T entity)
+        {
+            var filter = Builders<T>.Filter.Eq(idField, idValue);
+            await _collection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task DeleteAsync(string idField, string idValue)
+        {
+            var filter = Builders<T>.Filter.Eq(idField, idValue);
+            await _collection.DeleteOneAsync(filter);
+        }
+    }
+}
